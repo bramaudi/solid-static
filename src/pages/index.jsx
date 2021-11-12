d6f715f1a62359eb92bfbc45bf687b7a7b64f811
Brama Udi <bramaudi@users.noreply.github.com>
2021-11-12 07:46:15 +0700

    Initial commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,17 @@
+import Counter from "~/components/Counter";
+import { Link } from "solid-app-router"
+import "./index.css"
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello world!</h1>
+      <Counter />
+      <p>
+        Visit{" "}
+        <Link href="/blog/why-i-love-solid/">Another page</Link>{" "}
+        to learn how to build Solid apps.
+      </p>
+    </main>
+  );
+}
