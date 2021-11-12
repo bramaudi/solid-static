8c6a119219d31373448f14905571eca36955445c
Brama Udi <bramaudi@users.noreply.github.com>
2021-11-12 07:58:38 +0700

    Try fixing export script (2)

--- export.js
+++ export.js
@@ -1,9 +1,9 @@
 import { spawn, spawnSync, execSync } from 'child_process';
 import { existsSync, mkdirSync } from 'fs';
 
-// if (existsSync('static')) {
-// 	execSync('rm -rf static')
-// }
+if (existsSync('static')) {
+	execSync('rm -rf static')
+}
 
 mkdirSync('static')
 execSync('cp -r public/* static')
@@ -14,6 +14,7 @@ serve.stdout.on('data', data => {
 	if (data.toString().match(/3000/)) {
 		spawnSync('wget', ['-P', 'static/', '-nH', '--html-extension', '-e', 'robots=off', '-r', 'localhost:3000']);
 		console.log('Done');
-		serve.kill('SIGHUP');
+		serve.kill();
+		process.exit(0)
 	}
 })
